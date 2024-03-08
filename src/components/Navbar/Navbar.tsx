1523d4d5d77a083d2880fea9333a5ef0550f1912
maxi326519 <maxi.326519@gmail.com>
2024-03-08 14:47:55 -0300

    Update Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,10 +11,18 @@ interface Props {
 
 export default function Navbar({ opaque }: Props) {
   const [showMenu, setShowMenu] = useState(false);
+  const [showProfileMenu, setShowProfileMenu] = useState(false);
+  const user = true;
 
   function handleShowMenu() {
     setShowMenu(!showMenu);
   }
+
+  function handleShowProfileMenu() {
+    setShowProfileMenu(!showProfileMenu);
+  }
+
+  function handleCloseSesion() {}
 
   return (
     <div
@@ -49,14 +57,33 @@ export default function Navbar({ opaque }: Props) {
             <Link to="/form">Contacto</Link>
           </li>
         </ul>
-        <ul className={styles.sesion}>
-          <li>
-            <Link to="/login">Iniciar sesión</Link>
-          </li>
-          <li>
-            <Link to="/signin">Registrarse</Link>
-          </li>
-        </ul>
+        {user ? (
+          <div className={styles.profile}>
+            <button onClick={handleShowProfileMenu}>Maximiliano</button>
+            <ul
+              className={`${styles.profileMenu} ${
+                showProfileMenu ? styles.showProfileMenu : ""
+              }`}
+            >
+              <li>
+                <Link to="/profile">Perfil</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li onClick={handleCloseSesion}>Cerrar sesión</li>
+            </ul>
+          </div>
+        ) : (
+          <ul className={styles.sesion}>
+            <li>
+              <Link to="/login">Iniciar sesión</Link>
+            </li>
+            <li>
+              <Link to="/signin">Registrarse</Link>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
---
+++ src/pages/Search/SearchDetails/SearchDetails.tsx
@@ -0,0 +1,9 @@
+import styles from "./SearchDetails.module.css";
+
+export default function SearchDetails() {
+  return (
+    <div>
+      <div className={styles.content}></div>
+    </div>
+  );
+}
